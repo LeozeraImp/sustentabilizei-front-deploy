c28ed53c44ffb20ad4354fbaf7441fa772becdc2
Leonardo Imperatori <leonardo.imperatori12@gmail.com>
2021-12-20 13:06:46 -0300

    Sobre Nos Atualizado

--- src/paginas/sobreNos/SobreNos.tsx
+++ src/paginas/sobreNos/SobreNos.tsx
@@ -2,35 +2,155 @@ import React, { useEffect } from "react";
 import { Box, Grid, Typography } from "@material-ui/core";
 import "./SobreNos.css";
 
-
 function SobreNos() {
+  return (
+    <>
+      <Grid item xs={12} className="corpo">
+        <Box padding={5}className="corpo1">
+          <Typography variant="h3" align="center" className="sus-titulo">
+            Sobre Nossa Historia
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12}className="corpo">
+        <Box display="flex" justifyContent="center" padding={2}>
+          <Box className="holder"></Box>
+          <Box className="fontes">
+            <img
+              src="https://i.imgur.com/2NPUk9V.jpg"
+              alt="Dan"
+              className="fotos"
+            />
+          </Box>
+          <Box className="fontes.a">
+            <Typography variant="h6" className="sus-titulo">
+              {" "}
+              A SE INTERESSAR PELA SUSTENTABILIDADE Por meio de parcerias , a
+              SUSTENTABILIZEI Quer fazer parte dessa próxima geração e
+              influenciar a conhecer o que há de mais moderno na área de
+              sustentabilidade. Nossas parcerias alcançam e educam ao redor do
+              mundo Trabalhar com Redes-Sociais para tornar a sustentabilidade
+              parte de nosso futuro coletivo é uma iniciativa que promove a
+              prosperidade e assegura nosso futuro.
+            </Typography>
+          </Box>
+        </Box>
+        <Box display="flex" justifyContent="center" padding={2}>
+          <Box className="holder"></Box>
+          <Box className="fontes">
+            <img
+              src="https://i.imgur.com/tMvk613.jpg"
+              alt="leandro"
+              className="fotos"
+            />
+          </Box>
+          <Box className="fontes.a">
+            <Typography variant="h6" className="sus-titulo">
+              {" "}
+              A SE INTERESSAR PELA SUSTENTABILIDADE Por meio de parcerias , a
+              SUSTENTABILIZEI Quer fazer parte dessa próxima geração e
+              influenciar a conhecer o que há de mais moderno na área de
+              sustentabilidade. Nossas parcerias alcançam e educam ao redor do
+              mundo Trabalhar com Redes-Sociais para tornar a sustentabilidade
+              parte de nosso futuro coletivo é uma iniciativa que promove a
+              prosperidade e assegura nosso futuro.
+            </Typography>
+          </Box>
+        </Box>
+        <Box display="flex" justifyContent="center" padding={2}>
+          <Box className="holder"></Box>
+          <Box className="fontes">
+            <img
+              src="https://i.imgur.com/fGh6bO2.jpg"
+              alt="Leo"
+              className="fotos"
+            />
+          </Box>
+          <Box className="fontes.a">
+            <Typography variant="h6" className="sus-titulo">
+              {" "}
+              A SE INTERESSAR PELA SUSTENTABILIDADE Por meio de parcerias , a
+              SUSTENTABILIZEI Quer fazer parte dessa próxima geração e
+              influenciar a conhecer o que há de mais moderno na área de
+              sustentabilidade. Nossas parcerias alcançam e educam ao redor do
+              mundo Trabalhar com Redes-Sociais para tornar a sustentabilidade
+              parte de nosso futuro coletivo é uma iniciativa que promove a
+              prosperidade e assegura nosso futuro.
+            </Typography>
+          </Box>
+        </Box>
 
-    return (
-        <>
-        <Grid item xs={12}>
-                    <Box padding={5}>
-                        <Typography variant="h3" align="center" className="sus-titulo">Sobre Nossa Historia</Typography>
-                    </Box>
-                </Grid>
+        <Box display="flex" justifyContent="center" padding={2}>
+          <Box className="holder"></Box>
+          <Box className="fontes">
+            <img
+              src="https://i.imgur.com/EuzHjCE.png"
+              alt="Lin"
+              className="fotos"
+            />
+          </Box>
+          <Box className="fontes.a">
+            <Typography variant="h6" className="sus-titulo">
+              {" "}
+              A SE INTERESSAR PELA SUSTENTABILIDADE Por meio de parcerias , a
+              SUSTENTABILIZEI Quer fazer parte dessa próxima geração e
+              influenciar a conhecer o que há de mais moderno na área de
+              sustentabilidade. Nossas parcerias alcançam e educam ao redor do
+              mundo Trabalhar com Redes-Sociais para tornar a sustentabilidade
+              parte de nosso futuro coletivo é uma iniciativa que promove a
+              prosperidade e assegura nosso futuro.
+            </Typography>
+          </Box>
+        </Box>
 
-     <Grid item xs={12}>
-                    <Box display="flex" justifyContent="center"padding={2}>
-                    <Box className="holder">
-                    </Box>
-                        <Box className="fontes" >
-                            <Typography variant="h5" align="center" color="primary" className="sus-titulo">Danilo</Typography>
-                        </Box>
-                        <Box className="fontes.a">
-                            <Typography variant="h6" > A SE INTERESSAR PELA SUSTENTABILIDADE
-Por meio de parcerias , a SUSTENTABILIZEI Quer fazer parte dessa próxima geração e influenciar a conhecer o que há de mais moderno na área de sustentabilidade. Nossas parcerias alcançam e educam ao redor do mundo Trabalhar com Redes-Sociais para tornar a sustentabilidade parte de nosso futuro coletivo é uma iniciativa que promove a prosperidade e assegura nosso futuro.</Typography>
-                        </Box>
+        <Box display="flex" justifyContent="center" padding={2}>
+          <Box className="holder"></Box>
+          <Box className="fontes">
+            <img
+              src="https://i.imgur.com/eNjDct9.jpg"
+              alt="Matheus"
+              className="fotos"
+            />
+          </Box>
+          <Box className="fontes.a">
+            <Typography variant="h6" className="sus-titulo">
+              {" "}
+              A SE INTERESSAR PELA SUSTENTABILIDADE Por meio de parcerias , a
+              SUSTENTABILIZEI Quer fazer parte dessa próxima geração e
+              influenciar a conhecer o que há de mais moderno na área de
+              sustentabilidade. Nossas parcerias alcançam e educam ao redor do
+              mundo Trabalhar com Redes-Sociais para tornar a sustentabilidade
+              parte de nosso futuro coletivo é uma iniciativa que promove a
+              prosperidade e assegura nosso futuro.
+            </Typography>
+          </Box>
+        </Box>
 
-                    </Box>
-                </Grid>
-
+        <Box display="flex" justifyContent="center" padding={2}>
+          <Box className="holder"></Box>
+          <Box className="fontes">
+            <img
+              src="https://i.imgur.com/wK62Lmb.jpg"
+              alt="Safira"
+              className="fotos"
+            />
+          </Box>
+          <Box className="fontes.a">
+            <Typography variant="h6" className="sus-titulo">
+              {" "}
+              A SE INTERESSAR PELA SUSTENTABILIDADE Por meio de parcerias , a
+              SUSTENTABILIZEI Quer fazer parte dessa próxima geração e
+              influenciar a conhecer o que há de mais moderno na área de
+              sustentabilidade. Nossas parcerias alcançam e educam ao redor do
+              mundo Trabalhar com Redes-Sociais para tornar a sustentabilidade
+              parte de nosso futuro coletivo é uma iniciativa que promove a
+              prosperidade e assegura nosso futuro.
+            </Typography>
+          </Box>
+        </Box>
+      </Grid>
     </>
-
-    );
+  );
 }
 
 export default SobreNos;
